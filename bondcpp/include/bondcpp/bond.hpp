c39928e7a3c96de2141b44a2c9249548b673c924
Csaba Sipos <siposcsaba89@gmail.com>
2021-01-12 11:22:32 +0100

    windows fixes
    Signed-off-by: Csaba Sipos <siposcsaba89@gmail.com>

--- bondcpp/include/bondcpp/bond.hpp
+++ bondcpp/include/bondcpp/bond.hpp
@@ -58,7 +58,7 @@ namespace bond
  * another process and be notified when it dies.  In turn, it will be
  * notified when you die.
  */
-class Bond
+class RCLCPP_EXPORT Bond
 {
 public:
   /** \brief Constructs a bond, but does not connect
